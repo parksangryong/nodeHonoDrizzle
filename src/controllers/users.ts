811e65898fea7fd478b7315dc485fb4390446c68
parksangryong <parksangryong@Mac-mini-2.local>
2025-03-26 09:27:32 +0900

    유저 정리

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,10 +1,6 @@
 import { db } from "../db";
 import { users } from "../db/schema";
 
-export const createUser = async (name: string, age: number, email: string) => {
-  await db.insert(users).values({ name, age, email });
-};
-
 export const getUsers = async () => {
   return await db.select().from(users);
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,41 +1,12 @@
 import { Hono } from "hono";
-import { createUser, getUsers } from "../controllers/users";
-import { zValidator } from "@hono/zod-validator";
-import { z } from "zod";
-import { HTTPException } from "hono/http-exception";
+import { getUsers } from "../controllers/users";
 
 const app = new Hono();
-
-const userSchema = z.object({
-  name: z.string().min(2),
-  age: z.number().min(1),
-  email: z.string().email(),
-});
 
 app.get("/", async (c) => {
   const users = await getUsers();
 
   return c.json({ users });
 });
-
-app.post(
-  "/",
-  zValidator("json", userSchema, (result, c) => {
-    if (!result.success) {
-      throw new HTTPException(400, {
-        message: "데이터 형식이 올바르지 않습니다.",
-      });
-    }
-  }),
-  async (c) => {
-    const { name, age, email } = c.req.valid("json");
-    await createUser(name, age, email);
-
-    return c.json({
-      success: true,
-      message: "유저 생성에 성공했습니다.",
-    });
-  }
-);
 
 export default app;
