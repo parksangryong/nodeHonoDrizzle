ac0aa225dc52fea79b32058d7c98360736b232f4
parksangryong <parksangryong@Mac-mini-2.local>
2025-03-28 16:41:24 +0900

    파일업로드 수정

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { timing } from "hono/timing";
 import { serve } from "@hono/node-server";
-
+import { serveStatic } from "@hono/node-server/serve-static";
 // db
 import { connectionResult } from "./db";
 
@@ -26,6 +26,13 @@ app.use("*", cors()); // CORS 활성화
 app.use("*", logger()); // morgan과 유사한 로깅
 app.use("*", timing()); // response time 측정
 app.use("*", rateLimit()); // 요청 제한
+// uploads 폴더의 파일들을 정적으로 제공
+app.use(
+  "/uploads/*",
+  serveStatic({
+    root: ".", // 현재 디렉토리를 루트로 설정
+  })
+);
 
 connectionResult();
 errorHandler(app);
@@ -35,13 +42,12 @@ app.route("/auth", auth);
 
 // 그 다음 인증 미들웨어 적용 (로그인 이외의 모든 라우트에 대해)
 app.use("/users/*", authenticateToken);
-app.use("/uploads/*", authenticateToken);
 app.use("/boards/*", authenticateToken);
 app.use("/comments/*", authenticateToken);
 
 // 보호된 라우트들
 app.route("/users", users);
-app.route("/uploads", uploads);
+app.route("/files", uploads);
 app.route("/boards", board);
 app.route("/comments", comment);
 
--- src/routes/uploads.ts
+++ src/routes/uploads.ts
@@ -1,15 +1,16 @@
 import { Hono } from "hono";
-
 import { uploadFile } from "../controllers/uploads";
 import { downloadFile, listFiles, deleteFile } from "../controllers/image";
+import { authenticateToken } from "../middleware/auth.middleware";
+
 const app = new Hono();
 
-app.post("/", uploadFile);
+app.post("/upload", authenticateToken, uploadFile);
 
-app.get("/download/:id", downloadFile);
+app.get("/download/:id", authenticateToken, downloadFile);
 
-app.get("/list", listFiles);
+app.get("/list", authenticateToken, listFiles);
 
-app.delete("/:id", deleteFile);
+app.delete("/:id", authenticateToken, deleteFile);
 
 export default app;
