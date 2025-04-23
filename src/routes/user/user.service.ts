f1973799dffefa388b4476a2b44bf24da05f5648
parksangryong <ppsr3478@naver.com>
2025-04-23 10:50:50 +0900

    user

--- src/routes/user/user.service.ts
+++ src/routes/user/user.service.ts
@@ -1,15 +1,35 @@
 import { db } from "../../db";
 import { users } from "../../db/schema";
+import { UserResponse } from "./user.schema";
+import { eq } from "drizzle-orm";
+
+// constants
+import { Errors } from "../../constants/error";
+import { MySqlRawQueryResult } from "drizzle-orm/mysql2";
 
 export const createUser = async (
   name: string,
   age: number,
   email: string,
   password: string
-) => {
-  await db.insert(users).values({ name, age, email, password });
+): Promise<MySqlRawQueryResult> => {
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email));
+
+  if (existingUser.length > 0) {
+    throw new Error(Errors.USER.EMAIL_EXISTS.code);
+  }
+
+  return await db.insert(users).values({ name, age, email, password });
 };
 
-export const getUsers = async () => {
-  return await db.select().from(users);
+export const getUsers = async (): Promise<UserResponse> => {
+  const userList = await db.select().from(users);
+  return {
+    success: true,
+    message: "유저 목록 조회 성공",
+    data: userList,
+  };
 };
