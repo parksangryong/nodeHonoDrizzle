2a3381af0c9cae6603e54753584c3652853eae6b
parksangryong <ppsr3478@naver.com>
2025-05-14 10:50:37 +0900

    redis 토큰 완료

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,10 +1,15 @@
 import type { Context, Next } from "hono";
 
 // utils
-import { getAccessToken, verifyAccessToken } from "../utils/jwt";
+import {
+  getAccessToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+} from "../utils/jwt";
 
 // constants
 import { Errors } from "../constants/error";
+import { redis } from "./redis.middleware";
 
 interface JwtPayload {
   name: string;
@@ -23,6 +28,19 @@ export const authenticateToken = async (c: Context, next: Next) => {
 
   if (!decoded) {
     throw new Error(Errors.JWT.ACCESS_EXPIRED.code);
+  }
+
+  const refreshToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+  if (!refreshToken) {
+    throw new Error(Errors.JWT.INVALID_REFRESH_TOKEN.code);
+  }
+
+  const storedTokenData = JSON.parse(refreshToken);
+  const decoded_refreshToken = verifyRefreshToken(storedTokenData);
+
+  if (!decoded_refreshToken) {
+    throw new Error(Errors.JWT.REFRESH_EXPIRED.code);
   }
 
   await next();
